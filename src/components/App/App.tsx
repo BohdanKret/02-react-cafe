7aa369b967a2a13332a47a30f9a4b1872f8fa6be
Bohdan Kret <kretbohdan2002@gmail.com>
2025-07-31 10:14:59 +0300

    create and done my homework

---
+++ src/components/App/App.tsx
@@ -0,0 +1,49 @@
+import CafeInfo from "../CafeInfo/CafeInfo";
+import Notification from "../Notification/Notification";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import css from "./App.module.css";
+import { useState } from "react";
+
+interface Votes {
+  good: number;
+  neutral: number;
+  bad: number;
+}
+
+function App() {
+ const [votes, setVotes] = useState<Votes>({
+   good: 0,
+   neutral: 0,
+   bad: 0,
+ });
+  
+  function handleVote(type: keyof typeof votes) {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+  };
+
+  function resetVotes() {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes >= 1} />
+      {totalVotes >= 1 && <VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />}
+      {totalVotes === 0 && <Notification/>}
+    </div>
+  );
+}
+
+export default App;
